ec98e6ab75b6ad46040a3a4b89993d848ab18987
Caleb Hugo <caleb@calebhugo.com>
2021-02-28 19:40:31 -0500

    Questions to be asked based on an answer range.

--- src/redux/questions/math/reducer.js
+++ src/redux/questions/math/reducer.js
@@ -2,18 +2,24 @@ import { NEW_MATH_QUESTION } from "./actions";
 import { randomNumberBetween } from "util/math";
 
 class QuestionMaker {
-    constructor() {
+    constructor({ maxAnswer }) {
+        this.numbers = [];
+        this.maxAnswer = maxAnswer;
         this.newQuestion();
     }
 
     newQuestion = () => {
-        this.numbers = [randomNumberBetween(0, 10), randomNumberBetween(0, 10)];
+        this.numbers[0] = randomNumberBetween(0, this.maxAnswer);
+        this.numbers[1] = randomNumberBetween(
+            0,
+            this.maxAnswer - this.numbers[0]
+        );
         this.question = `${this.numbers[0]} + ${this.numbers[1]}`;
         this.answer = this.numbers[0] + this.numbers[1];
     };
 }
 
-const questionMaker = new QuestionMaker();
+const questionMaker = new QuestionMaker({ maxAnswer: 10 });
 
 const reducer = (
     state = { question: questionMaker.question, answer: questionMaker.answer },
