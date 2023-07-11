d6bbd2b6a86f3ca6b3a95bf38f1dfb2a0a2f8f97
Caleb Hugo <calebjedhugo@gmail.com>
2023-07-10 23:48:30 -0400

    A few more spelling words.

--- src/logic/reading/QuestionMaker.js
+++ src/logic/reading/QuestionMaker.js
@@ -1,18 +1,18 @@
-import { icons } from "./questions";
-import { randomNumberBetween } from "util/math";
+import { random } from 'lodash';
+import { icons } from './questions';
 
 const words = Object.keys(icons);
 
 class QuestionMaker {
-    constructor() {
-        this.newQuestion();
-    }
+  constructor() {
+    this.newQuestion();
+  }
 
-    newQuestion = () => {
-        const idx = randomNumberBetween(0, words.length - 1);
-        this.answer = words[idx];
-        this.question = words[idx];
-    };
+  newQuestion = () => {
+    const idx = random(0, words.length - 1);
+    this.answer = words[idx];
+    this.question = words[idx];
+  };
 }
 
 export default QuestionMaker;
--- src/logic/reading/questions.js
+++ src/logic/reading/questions.js
@@ -1,20 +1,26 @@
-import React from "react";
-import Left from "@material-ui/icons/ArrowBack";
-import Right from "@material-ui/icons/ArrowForward";
-import Down from "@material-ui/icons/ArrowDownward";
-import Up from "@material-ui/icons/ArrowUpward";
-import Boat from "@material-ui/icons/DirectionsBoat";
-import Door from "@material-ui/icons/MeetingRoom";
-import Bed from "@material-ui/icons/LocalHotel";
+import React from 'react';
+import Left from '@material-ui/icons/ArrowBack';
+import Right from '@material-ui/icons/ArrowForward';
+import Down from '@material-ui/icons/ArrowDownward';
+import Up from '@material-ui/icons/ArrowUpward';
+import Boat from '@material-ui/icons/DirectionsBoat';
+import Door from '@material-ui/icons/MeetingRoom';
+import Bed from '@material-ui/icons/LocalHotel';
+import { BugReport, Face, EventSeat, AirportShuttle, AttachMoney } from '@material-ui/icons';
 
 const icons = {
-    left: <Left fontSize="inherit" />,
-    right: <Right fontSize="inherit" />,
-    down: <Down fontSize="inherit" />,
-    up: <Up fontSize="inherit" />,
-    boat: <Boat fontSize="inherit" />,
-    door: <Door fontSize="inherit" />,
-    bed: <Bed fontSize="inherit" />,
+  left: <Left fontSize="inherit" />,
+  right: <Right fontSize="inherit" />,
+  down: <Down fontSize="inherit" />,
+  up: <Up fontSize="inherit" />,
+  boat: <Boat fontSize="inherit" />,
+  door: <Door fontSize="inherit" />,
+  bed: <Bed fontSize="inherit" />,
+  bug: <BugReport fontSize="inherit" />,
+  face: <Face fontSize="inherit" />,
+  chair: <EventSeat fontSize="inherit" />,
+  car: <AirportShuttle fontSize="inherit" />,
+  money: <AttachMoney fontSize="inherit" />,
 };
 
 export { icons };
