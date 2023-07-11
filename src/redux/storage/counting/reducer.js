ec84f5e6731724133e1be5f974c76e2fe31560b7
Caleb Hugo <calebjedhugo@gmail.com>
2023-07-10 22:53:55 -0400

    Better storage of counting data.

--- src/redux/storage/counting/reducer.js
+++ src/redux/storage/counting/reducer.js
@@ -1,13 +1,19 @@
+import { repeat } from 'lodash';
 import { PUSH_COUNTING_HISTORY, RESET_COUNTING_HISTORY } from './actions';
 import { countingHistory } from 'storage/localStorage';
 
 const countingReducer = (
   state = countingHistory.data,
-  { type, payload: { countingAnswer: answer, countingQuesion: question, ...rest } = {} }
+  { type, payload: { answer, question, ...rest } = {} }
 ) => {
   switch (type) {
     case PUSH_COUNTING_HISTORY:
-      const withTime = { answer, question, ...rest, time: new Date().getTime() };
+      const withTime = {
+        answer,
+        question: '*'.repeat(Number(answer)),
+        ...rest,
+        time: new Date().getTime(),
+      };
       countingHistory.push(withTime);
       return [...state, withTime];
     case RESET_COUNTING_HISTORY:
