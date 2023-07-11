a3038c37d462ee866db86e4ce4b5227dd6daeebd
Caleb Hugo <calebjedhugo@gmail.com>
2023-07-10 23:35:07 -0400

    Added more motivation.

--- src/components/Typing/Typing.jsx
+++ src/components/Typing/Typing.jsx
@@ -1,66 +1,61 @@
-import React, { useEffect, useRef, useState } from "react";
-import Input from "@material-ui/core/Input";
-import Typography from "@material-ui/core/Typography";
-import Box from "@material-ui/core/Box";
-import styled from "styled-components";
-import Motivator from "components/common/Motivator";
+import React, { useEffect, useRef, useState } from 'react';
+import Input from '@material-ui/core/Input';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import styled from 'styled-components';
+import Motivator from 'components/common/Motivator';
 
 const InvisbileBox = styled(Box)`
-    opacity: 0;
-    position: fixed;
+  opacity: 0;
+  position: fixed;
 `;
 
-const Typing = ({ pastChars, currentChar, futureChars, push, advance }) => {
-    const [addPoint, setAddPoint] = useState(false);
-    const [clearScore, setClearScore] = useState(false);
-    const handleStroke = (e) => {
-        if (e.target.value === currentChar) {
-            setAddPoint(true);
-            advance();
-        } else {
-            setClearScore(true);
-        }
+const Typing = ({ pastChars, currentChar, futureChars, advance }) => {
+  const [addPoint, setAddPoint] = useState(false);
+  const [clearScore, setClearScore] = useState(false);
+  const handleStroke = (e) => {
+    if (e.target.value === currentChar) {
+      setAddPoint(true);
+      advance();
+    } else {
+      setClearScore(true);
+    }
+  };
+  const inputRef = useRef();
+  useEffect(() => {
+    window.addEventListener('click', () => {
+      inputRef.current?.click();
+    });
+    const localRef = inputRef;
+    // returned function will be called on component unmount
+    return () => {
+      window.removeEventListener('click', () => {
+        localRef.current?.click();
+      });
     };
-    const inputRef = useRef();
-    useEffect(() => {
-        window.addEventListener("click", () => {
-            inputRef.current?.click();
-        });
-        const localRef = inputRef;
-        // returned function will be called on component unmount
-        return () => {
-            window.removeEventListener("click", () => {
-                localRef.current?.click();
-            });
-        };
-    }, []);
-    const commonProps = { variant: "body2", display: "inline" };
-    return (
-        <>
-            <InvisbileBox>
-                <Input
-                    autoFocus={true}
-                    ref={inputRef}
-                    onChange={handleStroke}
-                    value={""}
-                />
-            </InvisbileBox>
-            <Box display="inline">
-                <Typography {...commonProps} color="textSecondary">
-                    {pastChars}
-                </Typography>
-                <Typography {...commonProps} color="primary">
-                    {currentChar}
-                </Typography>
-                <Typography {...commonProps}>{futureChars}</Typography>
-            </Box>
-            <Motivator
-                addPoint={addPoint}
-                setAddPoint={setAddPoint}
-                clearScore={clearScore}
-                setClearScore={setClearScore}
-            />
-        </>
-    );
+  }, []);
+  const commonProps = { variant: 'body2', display: 'inline' };
+  return (
+    <>
+      <InvisbileBox>
+        <Input autoFocus={true} ref={inputRef} onChange={handleStroke} value={''} />
+      </InvisbileBox>
+      <Box display="inline">
+        <Typography {...commonProps} color="textSecondary">
+          {pastChars}
+        </Typography>
+        <Typography {...commonProps} color="primary">
+          {currentChar}
+        </Typography>
+        <Typography {...commonProps}>{futureChars}</Typography>
+      </Box>
+      <Motivator
+        addPoint={addPoint}
+        setAddPoint={setAddPoint}
+        clearScore={clearScore}
+        setClearScore={setClearScore}
+      />
+    </>
+  );
 };
 export default Typing;
--- src/components/common/Motivator/Motivator.jsx
+++ src/components/common/Motivator/Motivator.jsx
@@ -1,8 +1,9 @@
-import { useEffect, useState } from "react";
-import Box from "@material-ui/core/Box";
-import PropTypes from "prop-types";
-import Paper from "@material-ui/core/Paper";
-import styles from "styled-components";
+import { useEffect, useState } from 'react';
+import Box from '@material-ui/core/Box';
+import PropTypes from 'prop-types';
+import Paper from '@material-ui/core/Paper';
+import styles from 'styled-components';
+import Typography from '@material-ui/core/Typography';
 
 const SBox = styles(Box)`
     transition: height 1s;
@@ -11,52 +12,59 @@ const SBox = styles(Box)`
 let penaltyInterval;
 
 const Motivator = ({
-    maxScore = 10,
-    penaltyTimout = 5000,
-    addPoint,
-    setAddPoint,
-    clearScore,
-    setClearScore,
+  maxScore = 10,
+  penaltyTimout = 5000,
+  addPoint,
+  setAddPoint,
+  clearScore,
+  setClearScore,
 }) => {
-    const [score, setScore] = useState(0);
-    useEffect(() => {
-        if (addPoint) {
-            setScore(Math.min(score + 1, maxScore));
-            clearInterval(penaltyInterval);
-            penaltyInterval = setInterval(() => {
-                setScore((score) => Math.max(score - 1, 0));
-            }, penaltyTimout);
-            setAddPoint(false);
-        }
-    }, [addPoint, maxScore, penaltyTimout, score, setAddPoint]);
+  const [score, setScore] = useState(0);
+  const [bestStreak, setBestStreak] = useState(0);
+  const [streak, setStreak] = useState(0);
+  useEffect(() => {
+    if (addPoint) {
+      setScore(Math.min(score + 1, maxScore));
+      clearInterval(penaltyInterval);
+      penaltyInterval = setInterval(() => {
+        setScore((score) => Math.max(score - 1, 0));
+      }, penaltyTimout);
+      setAddPoint(false);
+      if (score === maxScore) {
+        setStreak(streak + 1);
+        setBestStreak(Math.max(bestStreak, streak + 1));
+      }
+    }
+  }, [addPoint, bestStreak, maxScore, penaltyTimout, score, setAddPoint, streak]);
 
-    useEffect(() => {
-        if (clearScore) {
-            setClearScore(false);
-            setScore(0);
-        }
-    }, [clearScore, setClearScore]);
-    return (
-        <Box position="fixed" bottom={10} right={10} width={20}>
-            <Paper elevation={5}>
-                <SBox
-                    bgcolor="primary.main"
-                    height={`${Math.ceil((score / maxScore) * 90)}vh`}
-                />
-            </Paper>
-        </Box>
-    );
+  useEffect(() => {
+    if (clearScore) {
+      setClearScore(false);
+      setScore(0);
+      setStreak(0);
+    }
+  }, [clearScore, setClearScore]);
+  return (
+    <Box position="fixed" bottom={10} right={10} width={100} zIndex={-1}>
+      <Typography noWrap>
+        {streak} / {bestStreak}
+      </Typography>
+      <Paper elevation={5}>
+        <SBox bgcolor="secondary.main" height={`${Math.ceil((score / maxScore) * 80)}vh`} />
+      </Paper>
+    </Box>
+  );
 };
 
 Motivator.propTypes = {
-    /** The score required to have the Motivator at its largest size. */
-    maxScore: PropTypes.number,
-    /** The number of ms is takes for one point to be removed. */
-    pentaltyInterval: PropTypes.number,
-    /** Pass in true if a point should be added. */
-    addPoint: PropTypes.bool.isRequired,
-    /** Pass in the function for setting addPoint. It is set to false once the interval is reset */
-    setAddPoint: PropTypes.func.isRequired,
+  /** The score required to have the Motivator at its largest size. */
+  maxScore: PropTypes.number,
+  /** The number of ms is takes for one point to be removed. */
+  pentaltyInterval: PropTypes.number,
+  /** Pass in true if a point should be added. */
+  addPoint: PropTypes.bool.isRequired,
+  /** Pass in the function for setting addPoint. It is set to false once the interval is reset */
+  setAddPoint: PropTypes.func.isRequired,
 };
 
 export default Motivator;
