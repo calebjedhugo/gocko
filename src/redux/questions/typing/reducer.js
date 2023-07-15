45a100a31b25e856c79d6df4aca124c64437e10c
Caleb Hugo <calebjedhugo@gmail.com>
2023-07-15 09:46:52 -0400

    Added s and l

--- src/redux/questions/typing/reducer.js
+++ src/redux/questions/typing/reducer.js
@@ -1,21 +1,21 @@
-import { ADVANCE_TYPING_STRING, SET_TYPING_CHARS } from "./actions";
-import StringMaker from "logic/typing/StringMaker";
+import { ADVANCE_TYPING_STRING, SET_TYPING_CHARS } from './actions';
+import StringMaker from 'logic/typing/StringMaker';
 
 // TODO: initialize from local storage.
-const stringMaker = new StringMaker({ typingChars: "fjdk" });
+const stringMaker = new StringMaker({ typingChars: 'fjdksl' });
 
 const reducer = (state = stringMaker.state, { type, payload }) => {
-    switch (type) {
-        case ADVANCE_TYPING_STRING:
-            stringMaker.advance();
-            return { ...state, ...stringMaker.state };
-        case SET_TYPING_CHARS:
-            const { value } = payload || {};
-            stringMaker.currentChars = value;
-            return { ...state, ...stringMaker.state };
-        default:
-            return state;
-    }
+  switch (type) {
+    case ADVANCE_TYPING_STRING:
+      stringMaker.advance();
+      return { ...state, ...stringMaker.state };
+    case SET_TYPING_CHARS:
+      const { value } = payload || {};
+      stringMaker.currentChars = value;
+      return { ...state, ...stringMaker.state };
+    default:
+      return state;
+  }
 };
 
 export default reducer;
